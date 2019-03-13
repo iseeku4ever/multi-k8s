0240e092bd0d886ed7c229bde89a06882a466ee9
Abinash.Sahu <Abinash.Sahu@synechron.com>
2019-03-13 23:24:32 +0530

    initial version2

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
-docker build -t stephengrider/multi-client:latest -t stephengrider/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t stephengrider/multi-server:latest -t stephengrider/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t stephengrider/multi-worker:latest -t stephengrider/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t iseeku4ever/multi-client:latest -t iseeku4ever/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t iseeku4ever/multi-server:latest -t iseeku4ever/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t iseeku4ever/multi-worker:latest -t iseeku4ever/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-docker push stephengrider/multi-client:latest
-docker push stephengrider/multi-server:latest
-docker push stephengrider/multi-worker:latest
+docker push iseeku4ever/multi-client:latest
+docker push iseeku4ever/multi-server:latest
+docker push iseeku4ever/multi-worker:latest
 
-docker push stephengrider/multi-client:$SHA
-docker push stephengrider/multi-server:$SHA
-docker push stephengrider/multi-worker:$SHA
+docker push iseeku4ever/multi-client:$SHA
+docker push iseeku4ever/multi-server:$SHA
+docker push iseeku4ever/multi-worker:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=stephengrider/multi-server:$SHA
-kubectl set image deployments/client-deployment client=stephengrider/multi-client:$SHA
-kubectl set image deployments/worker-deployment worker=stephengrider/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=iseeku4ever/multi-server:$SHA
+kubectl set image deployments/client-deployment client=iseeku4ever/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=iseeku4ever/multi-worker:$SHA
